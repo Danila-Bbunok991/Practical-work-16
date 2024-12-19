24833ac10ba3d954c5dd8e18ed071f2af27380bf
Danila <danila230304@icloud.com>
2024-12-19 17:25:28 +0300

    Initial Commit

---
+++ Practical_work_16/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Practical_work_16
+//
+//  Created by Danila Moiseenkov on 12/19/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
